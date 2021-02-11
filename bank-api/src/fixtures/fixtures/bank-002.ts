7a6ddf6786aa38e668296e882bd751683f6e4e25
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-02-10 23:56:17 -0300

    Feat: Created fake data for bank 002 cter

--- bank-api/src/fixtures/fixtures/bank-002.ts
+++ bank-api/src/fixtures/fixtures/bank-002.ts
@@ -1,18 +1,18 @@
 export default [
-    {
-      model: 'BankAccount',
+  {
+    model: "BankAccount",
+    fields: {
+        id: "103cc632-78e7-4476-ab63-d5ad3a562d26",
+        account_number: "3333",
+        owner_name: "User CTER 1",
+    }
+  },
+  {
+      model: "BankAccount",
       fields: {
-        id: '6e4635ce-88d1-4e58-9597-d13fc446ee47',
-        account_number: '1111',
-        owner_name: 'User BBX 1',
-      },
-    },
-    {
-      model: 'BankAccount',
-      fields: {
-        id: '51a720b2-5144-4d7f-921d-57023b1e24c1',
-        account_number: '2222',
-        owner_name: 'User BBX 2',
-      },
-    },
+          id: "463b1b2a-b5fa-4b88-9c31-e5c894a20ae3",
+          account_number: "4444",
+          owner_name: "User CTER 2",
+      }
+  },
   ];
