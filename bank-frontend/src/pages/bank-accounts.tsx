c068c17c855e4df0a1018ee6296018e25a88642c
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-02-16 16:04:23 -0300

    Created bank account cards component

--- bank-frontend/src/pages/bank-accounts.tsx
+++ bank-frontend/src/pages/bank-accounts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BankAccountCard from '../components/BankAccountCard';
 import Layout from '../components/Layout';
 import Title from '../components/Title';
 
@@ -10,6 +11,17 @@ const BankAccountsList = (props: Props) => {
     return (
         <Layout>
             <Title>Contas bancárias</Title>
+            <div className="row">
+                <a className="col-12 col-sm-6 col-md4">
+                    <BankAccountCard bankAccount={{id: 'teste1', owner_name: 'teste1', balance: 0, account_number: '11111'}}/>
+                </a>
+                <a className="col-12 col-sm-6 col-md4">
+                    <BankAccountCard bankAccount={{id: 'teste2', owner_name: 'teste2', balance: 0, account_number: '22222'}}/>
+                </a>
+                <a className="col-12 col-sm-6 col-md4">
+                    <BankAccountCard bankAccount={{id: 'teste3', owner_name: 'teste3', balance: 0, account_number: '33333'}}/>
+                </a>
+            </div>
         </Layout>
     );
 };
