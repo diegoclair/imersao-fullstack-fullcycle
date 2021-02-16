353f62a6d67082b7d67cbe1c2b8f6eaa51b2c935
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-02-16 17:26:02 -0300

    Created link in our navbar to go to our bank list

--- bank-frontend/src/components/Navbar/index.tsx
+++ bank-frontend/src/components/Navbar/index.tsx
@@ -1,16 +1,19 @@
+import Link from 'next/link';
 import classes from './Navbar.module.scss'
 
 const Navbar: React.FC = (props) => {
     return (
         <nav className={`navbar navbar-expand-lg ${classes.root} ${classes.bank001}`}>
             <div className={`container-fluid ${classes.navbarBody}`}>
-                <a className={`navbar-brand ${classes.navbarBrand}`} href="#">
-                    <img src="/img/icon_banco.png" alt="" className={classes.logoBank}/>
-                    <div className={classes.bankName}>
-                        <span>Cod - 001</span>
-                        <h2>BBX</h2>
-                    </div>
-                </a>
+                <Link href={`/bank-accounts`}>
+                    <a className={`navbar-brand ${classes.navbarBrand}`} href="#">
+                        <img src="/img/icon_banco.png" alt="" className={classes.logoBank}/>
+                        <div className={classes.bankName}>
+                            <span>Cod - 001</span>
+                            <h2>BBX</h2>
+                        </div>
+                    </a>
+                    </Link>
                 <div className={`collapse navbar-collapse ${classes.navbarRightRoot}`} id="navbarSupportedContent">
                     <ul className={`navbar-nav ml-auto ${classes.navbarRightBody}`}>
                         <li className={`nav-item ${classes.bankAccountInfo}`}>
