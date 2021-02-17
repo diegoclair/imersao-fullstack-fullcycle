e4c505fc029acefe0b081ac73b952d248a0064ac
Diego Clair Rodrigues <diego93rodrigues@gmail.com>
2021-02-16 21:36:42 -0300

    fix cors error

--- bank-api/src/main.ts
+++ bank-api/src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 import { ModelNotFoundExceptionFilter } from './exception-filters/model-not-found.exception-filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //{cors: true} is to open for all hosts
+  const app = await NestFactory.create(AppModule, {cors: true});
   app.setGlobalPrefix('api')
   app.useGlobalFilters(new ModelNotFoundExceptionFilter());
   app.connectMicroservice({
